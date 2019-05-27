0a0a1b247cb142d4ae174dbe05821d904f547ba0
tobiaslocker <tobias@tobiaslocker.de>
2019-05-27 23:47:26 +0100

    Initial commit

---
+++ include/base64.hpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <string>
+
+std::string to_base64(std::string const &data) {
+  int counter = 0;
+  uint32_t bit_stream = 0;
+
+  std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                             "abcdefghijklmnopqrstuvwxyz"
+                             "0123456789+/";
+
+  std::string base64;
+  int offset = 0;
+  std::for_each(data.begin(), data.end(), [&](unsigned char c) {
+    auto num_val = static_cast<unsigned int>(c);
+    offset = 16 - counter % 3 * 8;
+    bit_stream += num_val << offset;
+    if (offset == 0 && counter != 3) {
+      base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
+      base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
+      base64 += base64_chars.at(bit_stream >> 6 & 0x3f);
+      base64 += base64_chars.at(bit_stream & 0x3f);
+      bit_stream = 0;
+    }
+    counter++;
+  });
+  if (offset == 16) {
+    base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
+    base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
+    base64 += '=';
+    base64 += '=';
+  }
+  if (offset == 8) {
+    base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
+    base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
+    base64 += base64_chars.at(bit_stream >> 6 & 0x3f);
+    base64 += '=';
+  }
+  return base64;
+}
---
+++ scripts/run-clang-format.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#clang-format -verbose -i -style=file src/*.cpp
+#clang-format -verbose -i -style=file test/*.cpp
+clang-format -verbose -i -style=file include/*.hpp
