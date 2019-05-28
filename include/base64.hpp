c24ad6469c2f56e1a91f41dbd18f6afadb7e5216
tobiaslocker <tobias@tobiaslocker.de>
2019-05-28 17:16:44 +0100

    Shrink algorithm

--- include/base64.hpp
+++ include/base64.hpp
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <string>
 
+#include <bitset>
+#include <iostream>
+
 std::string to_base64(std::string const &data) {
   int counter = 0;
   uint32_t bit_stream = 0;
@@ -15,24 +18,28 @@ std::string to_base64(std::string const &data) {
     auto num_val = static_cast<unsigned int>(c);
     offset = 16 - counter % 3 * 8;
     bit_stream += num_val << offset;
-    if (offset == 0 && counter != 3) {
+
+    if (offset == 16) {
       base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
+    }
+
+    if (offset == 8) {
       base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
+    }
+    if (offset == 0 && counter != 3) {
       base64 += base64_chars.at(bit_stream >> 6 & 0x3f);
       base64 += base64_chars.at(bit_stream & 0x3f);
       bit_stream = 0;
     }
     counter++;
   });
+
   if (offset == 16) {
-    base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
     base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
     base64 += '=';
     base64 += '=';
   }
   if (offset == 8) {
-    base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
-    base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
     base64 += base64_chars.at(bit_stream >> 6 & 0x3f);
     base64 += '=';
   }
