7e1167c469aef09393e7bf57a37563b3f6f6d1f5
tobiaslocker <tobias@tobiaslocker.de>
2019-05-28 18:39:03 +0100

    Add decode function

--- include/base64.hpp
+++ include/base64.hpp
@@ -1,47 +1,69 @@
 #include <algorithm>
 #include <string>
 
-#include <bitset>
-#include <iostream>
-
 std::string to_base64(std::string const &data) {
   int counter = 0;
   uint32_t bit_stream = 0;
-
-  std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                             "abcdefghijklmnopqrstuvwxyz"
-                             "0123456789+/";
-
-  std::string base64;
+  const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                   "abcdefghijklmnopqrstuvwxyz"
+                                   "0123456789+/";
+  std::string encoded;
   int offset = 0;
   std::for_each(data.begin(), data.end(), [&](unsigned char c) {
     auto num_val = static_cast<unsigned int>(c);
     offset = 16 - counter % 3 * 8;
     bit_stream += num_val << offset;
-
     if (offset == 16) {
-      base64 += base64_chars.at(bit_stream >> 18 & 0x3f);
+      encoded += base64_chars.at(bit_stream >> 18 & 0x3f);
     }
-
     if (offset == 8) {
-      base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
+      encoded += base64_chars.at(bit_stream >> 12 & 0x3f);
     }
     if (offset == 0 && counter != 3) {
-      base64 += base64_chars.at(bit_stream >> 6 & 0x3f);
-      base64 += base64_chars.at(bit_stream & 0x3f);
+      encoded += base64_chars.at(bit_stream >> 6 & 0x3f);
+      encoded += base64_chars.at(bit_stream & 0x3f);
       bit_stream = 0;
     }
     counter++;
   });
-
   if (offset == 16) {
-    base64 += base64_chars.at(bit_stream >> 12 & 0x3f);
-    base64 += '=';
-    base64 += '=';
+    encoded += base64_chars.at(bit_stream >> 12 & 0x3f);
+    encoded += "==";
   }
   if (offset == 8) {
-    base64 += base64_chars.at(bit_stream >> 6 & 0x3f);
-    base64 += '=';
+    encoded += base64_chars.at(bit_stream >> 6 & 0x3f);
+    encoded += '=';
   }
-  return base64;
+  return encoded;
+}
+
+std::string from_base64(std::string const &data) {
+  int counter = 0;
+  uint32_t bit_stream = 0;
+  std::string decoded;
+  int offset = 0;
+  const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                   "abcdefghijklmnopqrstuvwxyz"
+                                   "0123456789+/";
+  for (auto const &c : data) {
+    auto num_val = base64_chars.find(c);
+    if (num_val != std::string::npos) {
+      offset = 18 - counter % 4 * 6;
+      bit_stream += num_val << offset;
+      if (offset == 12) {
+        decoded += static_cast<char>(bit_stream >> 16 & 0xff);
+      }
+      if (offset == 6) {
+        decoded += static_cast<char>(bit_stream >> 8 & 0xff);
+      }
+      if (offset == 0 && counter != 4) {
+        decoded += static_cast<char>(bit_stream & 0xff);
+        bit_stream = 0;
+      }
+    } else if (c != '=') {
+      return std::string();
+    }
+    counter++;
+  }
+  return decoded;
 }
