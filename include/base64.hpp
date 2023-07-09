b09e6eb8be17dbe7a2e8dfbb23401ed67d2be697
Torbjörn Rathsman <milasudril@gmail.com>
2023-07-09 10:57:51 +0200

    counter should be size_t to allow for longer strings

--- include/base64.hpp
+++ include/base64.hpp
@@ -48,15 +48,14 @@ inline std::string to_base64(std::string const &data) {
 }
 
 inline std::string from_base64(std::string const &data) {
-  int counter = 0;
+  size_t counter = 0;
   uint32_t bit_stream = 0;
   std::string decoded;
-  int offset = 0;
   const std::string base64_chars = get_base64_chars();
   for (unsigned char c : data) {
-    auto num_val = base64_chars.find(c);
+    auto const num_val = base64_chars.find(c);
     if (num_val != std::string::npos) {
-      offset = 18 - counter % 4 * 6;
+      auto const offset = 18 - counter % 4 * 6;
       bit_stream += static_cast<uint32_t>(num_val) << offset;
       if (offset == 12) {
         decoded += static_cast<char>(bit_stream >> 16 & 0xff);
@@ -69,7 +68,7 @@ inline std::string from_base64(std::string const &data) {
         bit_stream = 0;
       }
     } else if (c != '=') {
-      return std::string();
+      return std::string{};
     }
     counter++;
   }
