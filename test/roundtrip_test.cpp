dc5b927054988ba765cd3db5f7ac70bfe0319f7a
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2024-02-14 08:43:45 +0000

    adding basic clang-tidy checks

--- test/roundtrip_test.cpp
+++ test/roundtrip_test.cpp
@@ -1,25 +1,46 @@
 #include <iostream>
 #include <type_traits>
+#include <vector>
 
 #include "../include/base64.hpp"
 
-int main() {
-  std::string original;
-  for (int i = 0; i < 1024; i++) {
-    original += static_cast<char>(std::rand());
-  }
+int runtests() {
+  const std::vector<int> lengths{5, 10, 100, 1024, 10000, 1000003};
+  int outcome = EXIT_SUCCESS;
 
   std::cout << "char is "
             << (std::is_signed<char>::value ? "signed" : "unsigned")
             << std::endl;
 
-  auto encoded = base64::to_base64(original);
-  auto s = base64::from_base64(encoded);
-  if (s == original) {
-    std::cout << "PASS!!\n";
-    return 0;
-  } else {
-    std::cout << "FAIL!!\n";
-    return 1;
+  for (auto& length : lengths) {
+    std::string original;
+    for (int i = 0; i < length; i++) {
+      original += static_cast<char>(std::rand());
+    }
+
+    auto encoded = base64::to_base64(original);
+    auto s = base64::from_base64(encoded);
+
+    if (s == original) {
+      std::cout << "Test passed with length " << length << std::endl;
+    } else {
+      std::cout << "Test FAILED with length " << length << std::endl;
+      outcome = EXIT_FAILURE;
+    }
+  }
+  return outcome;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+  try {
+    return runtests();
+  } catch (const std::exception& e) {
+    // standard exceptions
+    std::cout << "Caught std::exception in main: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  } catch (...) {
+    // everything else
+    std::cout << "Caught unknown exception in main" << std::endl;
+    return EXIT_FAILURE;
   }
 }
